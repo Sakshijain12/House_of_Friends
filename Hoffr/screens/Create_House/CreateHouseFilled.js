ab4b802fa91ca9e8833d767794cca97cbd193673
Sakshijain12 <jain.60@iitj.ac.in>
2022-06-11 15:57:59 +0530

    join send

---
+++ Hoffr/screens/Create_House/CreateHouseFilled.js
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import { View, Image, Text} from 'react-native';
+import styles from "./createHouseStyle";
+
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { useRoute } from '@react-navigation/native';
+
+
+export default function CreateHouseFilled() {
+  const route = useRoute ();
+  const myIcon = <Icon name="plus" size={15} color="black" style = {styles.icon}/>;
+  return (
+    <View style={styles.container}>
+      <View style = {styles.logo}>
+        <Image source={require('../../assets/Group31.png')}></Image>
+      </View>
+      <View style = {styles.back}>
+        <Text> {route.params.houseName} </Text>
+        <Text style = {styles.pic}></Text>
+        <Text style = {styles.newChannel}>Customise Channels</Text>
+        
+        <Text style = {styles.box}>Chat Channel <Text style = {styles.text}>{"("}Default{")"}</Text>{"\n"} Live Channel <Text style = {styles.text}>{"("}Default{")"}</Text></Text>
+        
+        <Text style = {styles.newChannel}>{myIcon} New Channel</Text>
+        <Text style= {styles.bton}> CREATE HOUSE</Text>
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
--- Hoffr/screens/Join_House/JoinHouse.js
+++ Hoffr/screens/Join_House/JoinHouse.js
@@ -1,8 +1,13 @@
+import { useNavigation } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { View, Text, TextInput, Image} from 'react-native';
 import styles from "./joinHouseStyle";
 
 export default function JoinHouse() {
+  const navigation = useNavigation();
+  const join = () => {
+    navigation.navigate('SendLinkScreen')
+  }
   return (
     <View style={styles.container}>
       <View style = {styles.logo}>
@@ -11,7 +16,7 @@ export default function JoinHouse() {
       <View style = {styles.back}>
           <Text style = {styles.email}>Add joining code</Text>
           <TextInput style = {styles.box} placeholder = "Code"></TextInput>
-          <Text style = {styles.code}>Join</Text>
+          <Text style = {styles.code} onPress = {join}>Join</Text>
           <Text style = {styles.or}>-or-</Text>
           <Text style = {styles.link}>Click invite link sent to you</Text>
       </View>
